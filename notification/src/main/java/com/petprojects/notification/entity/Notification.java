3bd3c51e9141287906a34d1953602b7ca2cb6c2f
vkoschman <207955@niuitmo.ru>
2023-03-24 22:41:32 +0300

    corrected configuration

--- notification/src/main/java/com/petprojects/notification/entity/Notification.java
+++ notification/src/main/java/com/petprojects/notification/entity/Notification.java
@@ -15,14 +15,8 @@ import java.time.LocalDateTime;
 public class Notification {
 
     @Id
-    @SequenceGenerator(
-            name = "notification_id_sequence",
-            sequenceName = "notification_id_sequence"
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "notification_id_sequence"
-    )
+    @SequenceGenerator(name = "notification_id_sequence", sequenceName = "notification_id_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "notification_id_sequence")
     private Integer notificationId;
     private Integer toCustomerId;
     private String toCustomerEmail;
